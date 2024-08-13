0afb3474d5d29b56a934c1758c3d8a5df8176728
femicura <78215049+femicura@users.noreply.github.com>
2024-08-13 23:20:44 +0200

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,16 @@
 // This is your main script file
+window.addEventListener(
+  "load",
+  () => {
+    const check = document.querySelector("#terminCheck");
+    const button = document.querySelector("#terminButton");
+    check.addEventListener("change", () => {
+      if (check.checked) {
+        button.classList.remove("disabled");
+      } else {
+        button.classList.add("disabled");
+      }
+    });
+  },
+  false
+);
